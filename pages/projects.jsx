6945e20e28633fbf420596466023656efdff311e
João Oliveira <deoliveira.jvm@gmail.com>
2026-03-03 23:03:39 -0300

    A greater foundation

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -5,23 +5,29 @@ import { getProjects } from "./api/projects";
 const ProjectsPage = ({ projects }) => {
   const next = projects["next"];
   const vite = projects["vite"];
-  const vanilla = projects["vanilla"];
 
   return (
     <>
       <h2 className={styles.title}>Projetos</h2>
       <p className={styles.paragraph}>
-        Projetos de destaque que desenvolvi partindo de um design (Figma ou
-        PNG). Para a lista completa, veja os repositórios fixados{" "}
-        <a href="https://www.github.com/jvmdo" target="_blank">
-          no meu perfil do GitHub
-        </a>
-        .
+        Projetos de destaque que desenvolvi seguindo os seguintes critérios:
       </p>
+      <ul className={styles.paragraph} style={{ marginInlineStart: "1.5rem" }}>
+        <li>Deve partir de um design (Figma ou PNG)</li>
+        <li>Deve ser responsivo, mobile-first e pixel-perfect</li>
+        <li>
+          Deve ter um README explicando o problema, como resolveu, o que
+          aprendeu, dificuldades e demais detalhes do projeto
+        </li>
+      </ul>
       <p className={styles.paragraph}>
-        Todos os apps/páginas são responsivos, mobile-first workflow. A maioria
-        utiliza TypeScript.
+        A maioria é escrita em TypeScript e há forte preocupação com
+        acessibilidade.
       </p>
+      {/* <p className={styles.paragraph}>
+        Para projetos com tag AI, há uma seção no README explicando tirei
+        proveito dessas ferramentas para acelerar o desenvolvimento do produto.
+      </p> */}
       <h3 className={styles.subtitle}>Next.js</h3>
       <div className={styles.container}>
         {next.map((project) => (
@@ -34,12 +40,13 @@ const ProjectsPage = ({ projects }) => {
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
-      <h3 className={styles.subtitle}>Vanilla</h3>
-      <div className={styles.container}>
-        {vanilla.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
+      <p className={styles.paragraph}>
+        Lista completa disponíveis nos repositórios fixados{" "}
+        <a href="https://www.github.com/jvmdo" target="_blank">
+          no meu perfil do GitHub
+        </a>
+        .
+      </p>
     </>
   );
 };
